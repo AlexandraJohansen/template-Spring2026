31301604cab4b015551c1ece838298eed5bf06d1
AlexandraJohansen <145623007+AlexandraJohansen@users.noreply.github.com>
2026-09-22 11:09:56 -0400

    Refactor functions for better performance and clarity

--- scalahw/scalahw.scala
+++ scalahw/scalahw.scala
@@ -1,24 +1,76 @@
+import scala.math._
+
 case class Neumaier(sum: Double, c: Double)
 
 object HW {
 
-    // note it specifies the input (n) and its type (Int) along with the output
-    // type List[Int] ( a list of integers)
-    def q1(n: Int):List[Int] = {
-        List(0) // returns something of the correct output type in order to compile
-    }
+  def q1(n: Int): List[Int] = {
+    List.tabulate(n)(i => (i + 1) * (i + 1))
+  }
 
-    // In order to get the code to compile, you have to do the same with the rest of the
-    // questions, and then fill in code to make them correct.
 
-    def q3(x: Seq[Double]): Double = {
-        1.0
-    }
+  def q2(n: Int): Vector[Double] = {
+    Vector.tabulate(n)(i => sqrt(i + 1))
+  }
 
-    // you fill in the rest
-    
-    def q6(x: Seq[(Double, Double)]): (Double, Double) = {
-        (1.0, 1.0)
+
+  def q3(x: Seq[Double]): Double = {
+    x.foldLeft(0.0)(_ + _)
+  }
+  def q4(x: Seq[Double]): Double = {
+    x.foldLeft(1.0)(_ * _)
+  }
+
+
+  def q5(x: Seq[Double]): Double = {
+    x.foldLeft(0.0)((acc, elem) => acc + log(elem))
+  }
+
+ 
+  def q6(x: Seq[(Double, Double)]): (Double, Double) = {
+    x.foldLeft((0.0, 0.0)) { case ((sum1, sum2), (a, b)) =>
+      (sum1 + a, sum2 + b)
     }
+  }
+
+  def q7(x: Seq[(Double, Double)]): (Double, Double) = {
+    x.foldLeft((0.0, Double.NegativeInfinity)) { case ((sum, maxVal), (a, b)) =>
+      (sum + a, max(maxVal, b))
+    }
+  }
+
+
+  def q8(n: Int): (Long, Long) = {
+    (1 to n).foldLeft((0L, 1L)) { case ((sum, prod), i) =>
+      (sum + i, prod * i)
+    }
+  }
+
+
+  def q9(x: Seq[Int]): Int = {
+    val evens = x.filter(_ % 2 == 0)
+    if (evens.isEmpty) 0
+    else evens.map(i => i * i).reduce(_ + _)
+  }
+
+
+  def q10(x: Seq[Double]): Double = {
+    val (totalSum, _) = x.foldLeft((0.0, 1.0)) { case ((accSum, idx), elem) =>
+      (accSum + elem * idx, idx + 1.0)
+    }
+    totalSum
+  }
+    def q11(x: Seq[Double]): Double = {
+    val finalState = x.foldLeft(Neumaier(0.0, 0.0)) { (state, elem) =>
+      val t = state.sum + elem
+      val deltaC = if (abs(state.sum) >= abs(elem)) {
+        (state.sum - t) + elem
+      } else {
+        (elem - t) + state.sum
+      }
+      Neumaier(t, state.c + deltaC)
+    }
+    finalState.sum + finalState.c
+  }
 
 }

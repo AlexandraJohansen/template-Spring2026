e7cebe99c7c41904f02e7f8083775ccc925ed63e
AlexandraJohansen <145623007+AlexandraJohansen@users.noreply.github.com>
2026-09-22 10:21:59 -0400

    Refactor main function and update saveit method

--- re_sparklab/q1/q1.scala
+++ re_sparklab/q1/q1.scala
@@ -1,31 +1,69 @@
-// don't forget imports
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
 
 object Q1 {
-  def main(args: Array[String]) = {  // this is the entry point to our code
-    // don't change this function
-    val sc = getSC()  // sc variable for your program, 
-    val myrdd = getRDD(sc) //  get the rdd
-    val result = doRetail(myrdd)  // main computation
-    saveit("result", result)  // save the rdd to HDFS, change the folder name
+  def main(args: Array[String]) = {
+    val sc = getSC()
+    val myrdd = getRDD(sc)
+    val result = doRetail(myrdd)
+    saveit("spark1output", result)
   }
 
-  def getSC(): SparkContext = { // get the spark context variable
+  def getSC(): SparkContext = {
+    val conf = new SparkConf().setAppName("Q1").setIfMissing("spark.master", "local[*]")
+    new SparkContext(conf)
   }
 
-  def getRDD(sc:SparkContext): RDD[String] = { 
+  def getRDD(sc: SparkContext): RDD[String] = {
+    sc.textFile("/datasets/retailtab")
   }
-
 
   def doRetail(input: RDD[String]): RDD[(String, Int)] = {
-       // this does the bulk of the work. 
+    // Drop the header line
+    val noHeader = input.filter(line => !line.startsWith("InvoiceNo"))
+
+    // Parse each line; fields: InvoiceNo=0, StockCode=1, Description=2,
+    //   Quantity=3, InvoiceDate=4, UnitPrice=5, CustomerID=6, Country=7
+    val parsed = noHeader
+      .map(line => line.split("\t", -1))
+      .filter(fields => fields.length >= 8)
+      .map(fields => (fields(7).trim, fields(2).trim))  // (country, description)
+
+    // Every country must appear in output even with count 0.
+    // Emit (country, 0) for every line to establish all countries,
+    // then union with (country, 1) only for lines where description has "ER".
+    val allCountries = parsed.map({ case (country, _)    => (country, 0) })
+    val erLines      = parsed
+      .filter({ case (_, desc) => desc.contains("ER") })
+      .map({ case (country, _) => (country, 1) })
+
+    allCountries
+      .union(erLines)
+      .reduceByKey(_ + _)
   }
+
   def getTestRDD(sc: SparkContext): RDD[String] = {
+    val lines = Seq(
+      "InvoiceNo\tStockCode\tDescription\tQuantity\tInvoiceDate\tUnitPrice\tCustomerID\tCountry",
+      "536365\tA\tWHITE HANGING HEART\t6\t2010\t2.55\t17850\tUnited Kingdom",  // no ER
+      "536365\tB\tERASER SET\t3\t2010\t1.25\t17850\tUnited Kingdom",           // has ER
+      "536366\tC\tSILVER TRAY\t2\t2010\t3.39\t17851\tFrance",                  // no ER
+      "536366\tD\tSUPER ERASER\t4\t2010\t2.10\t17851\tFrance",                 // has ER
+      "536367\tE\tWOODEN BOX\t5\t2010\t1.50\t17852\tGermany"                   // no ER; Germany must still appear with 0
+    )
+    sc.parallelize(lines)
   }
+
   def expectedOutput(sc: SparkContext): RDD[(String, Int)] = {
-  }
-  def saveit(name: String, counts: RDD[(String, Int)]) = { 
-    // save the RDD, see wordcount example
+    sc.parallelize(Seq(
+      ("United Kingdom", 1),
+      ("France",         1),
+      ("Germany",        0)
+    ))
   }
 
-
+  def saveit(name: String, counts: RDD[(String, Int)]) = {
+    counts.saveAsTextFile(name)
+  }
 }
